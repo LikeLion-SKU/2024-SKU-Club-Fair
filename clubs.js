cfa45b99a99ac86d2365deb911fef9da89d75d23
changhoyu <tyu9817@naver.com>
2024-03-19 17:16:42 +0900

    update clubinfo

--- clubs.js
+++ clubs.js
@@ -3,38 +3,178 @@ import test1 from './public/test1.jpg';
 
 export const clubs = [
   {
-    id: '멋사',
-    title: '멋사',
+    id: '1',
+    title: '멋쟁이사자처럼',
     type: 'IT',
     img: likelion,
     description: '멋사임',
+    activity:
+      '매주 1회 필참 파트별 트랙, 자율 스터디, 아이디어톤, 해커톤, 데모데이, 연합해커톤 등등 다양한 IT 주요활동을 경험 할 수 있습니다.',
+    representatives: [
+      {
+        part: '대표',
+        name: '황현진',
+        phone: '010-2044-3300',
+      },
+      {
+        part: '부대표',
+        name: '하경림',
+        phone: '010-4353-0366',
+      },
+    ],
+    sns: '인스타그램 (@likelion_skuniv)',
+    clubinfo: [
+      {
+        menu: [
+          {
+            name: '달고나',
+            price: '3000원',
+          },
+          {
+            name: '사탕',
+            price: '3000원',
+          },
+        ],
+        game: [
+          {
+            name: '동아리 유형 테스트',
+            price: '0원',
+          },
+          {
+            name: '운영진과 타자대결',
+            price: '0원',
+          },
+          {
+            name: '그림 퀴즈',
+            price: '0원',
+          },
+        ],
+      },
+    ],
   },
   {
-    id: '몰루',
-    title: '몰루',
-    type: 'art',
+    id: '2',
+    title: 'CCC',
+    type: 'religion',
     img: test1,
-    description: '몰루임',
+    description:
+      'Campus Cursade for Christ의 약어로 한국 대학생 선교회이자, 기독교 동아리입니다!',
+    activity:
+      '1 대 1 맨토링, 가족순모임, 목요 채플, 여름/겨울 수련회, 단기선교 등이 있습니다.',
+    representatives: [
+      {
+        part: '대표',
+        name: '김준영',
+        phone: '010-5392-6208',
+      },
+      {
+        part: '간사',
+        name: '이현우',
+        phone: '010-5027-0288',
+      },
+    ],
+    sns: '인스타그램 (@sku_ccc)',
+    clubinfo: [
+      {
+        menu: [
+          {
+            name: '팝콘',
+            price: '무료',
+          },
+        ],
+        game: [
+          {
+            name: '더 포 설문조사',
+            price: '0원',
+          },
+        ],
+      },
+    ],
   },
   {
-    id: '몰루1',
-    title: '몰루1',
+    id: '3',
+    title: '적시타',
     type: '사회',
     img: test1,
-    description: '몰루1임',
+    description:
+      '야구를 좋아한다면 혹은 다양한 학과 사람들과 낭만있는 대학생활을 즐기고 싶다면 누구든지 적시타의 문을 두드려라! 야구를 몰라도 장비가 없어도 "열정"만 있다면 누구나, 언제든 두팔벌려 환영합니다.',
+    activity:
+      'AUBL(아마추어수도권대학야구리그)참가, 친선경기, 훈련, 프로야구 직관, 소모임, OB와 교류, 각종 파티 및 회식',
+    representatives: [
+      {
+        part: '대표',
+        name: '이찬호',
+        phone: '010-4184-6214',
+      },
+      {
+        part: '부대표',
+        name: '강민교',
+        phone: '010-9039-5672',
+      },
+    ],
+    sns: '인스타그램 (@juksita_2024)',
+    clubinfo: [
+      {
+        menu: [
+          {
+            name: '소세지',
+            price: '3000원',
+          },
+          {
+            name: '소떡소떡',
+            price: '3000원',
+          },
+        ],
+        game: [
+          {
+            name: '페펙트 피쳐',
+            price: '1000원',
+          },
+          {
+            name: '스피드킹',
+            price: '1000원',
+          },
+        ],
+      },
+    ],
   },
   {
-    id: '몰루2',
-    title: '몰루2',
-    type: 'scholar',
+    id: '4',
+    title: '한국기독학생회IVF',
+    type: 'religion',
     img: test1,
-    description: '몰루2임',
-  },
-  {
-    id: '몰루3',
-    title: '몰루3',
-    type: 'gym',
-    img: test1,
-    description: '몰루3임',
+    description:
+      '한국기독학생회 IVF는 북서울 11 개 캠퍼스, 전국 150여 곳의 대학생과  활동하는 대학생 선교단체로, 대학생들이 그리스도의 제자로서  살도록 격려하는 공동체입니다.',
+    activity:
+      '소그룹모임, 북서울 지방 대학교 연합MT 및 모임, 성균관-서경 지부 모임, 교내활동, 수련회  ',
+    representatives: [
+      {
+        part: '대표',
+        name: '이요한',
+        phone: '010-6551-7686',
+      },
+      {
+        part: '부대표',
+        name: '유태영',
+        phone: '010-9552-7290',
+      },
+    ],
+    sns: '인스타그램 (@sku_ivf)',
+    clubinfo: [
+      {
+        menu: [
+          {
+            name: '어묵',
+            price: '무료',
+          },
+        ],
+        game: [
+          {
+            name: 'Q&A세션',
+            price: '0원',
+          },
+        ],
+      },
+    ],
   },
 ];
