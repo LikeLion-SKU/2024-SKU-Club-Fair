a4f09ddee816b4fade5f2a7e10b2bb72d4e4ad85
changhoyu <tyu9817@naver.com>
2024-03-26 01:30:43 +0900

    about 수정

--- src/components/InfoLine.jsx
+++ src/components/InfoLine.jsx
@@ -1,15 +1,51 @@
-export default function InfoLine({ name, src, img ,info}) 
-{
-    return(
-        <div style={{ width: '90%'}}>
-        <div style={{ display: 'flex', flexDirection:'column', width: '100%', height: '90px',justifyContent : 'center' ,backgroundImage :"url('/box_about.png')",backgroundSize :'100% 100%'}}>
-            <div style={{ display: 'flex',flexDirection:'row', width: '100%', height: '30px',alignItems: 'center', paddingLeft : '15px'}}>
-                <img src={img} style={{ width: '29px',height: '29px',marginLeft: '15px',clipPath:'circle(50%)' }} />
-                <p style={{ marginLeft: '11px',fontSize :'20px' }}>{name}</p>
-                <p style={{ marginLeft: '12px',fontSize :'10px' }}>{info}</p>
-            </div>
-            <p style={{ paddingLeft : '33px', fontFamily: 'BMJUA'  , margin : '0',fontSize:'3vw'}}>{src}</p>
-        </div> 
+export default function InfoLine({ name, src, img, info }) {
+  return (
+    <div style={{ width: '90%' }}>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          width: '100%',
+          height: '90px',
+          justifyContent: 'center',
+          backgroundImage: "url('/box_about.png')",
+          backgroundSize: '100% 100%',
+        }}
+      >
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'row',
+            width: '100%',
+            height: '30px',
+            alignItems: 'center',
+            paddingLeft: '15px',
+          }}
+        >
+          <img
+            src={img}
+            style={{
+              width: '29px',
+              height: '29px',
+              marginLeft: '15px',
+              clipPath: 'circle(50%)',
+            }}
+          />
+          <p style={{ marginLeft: '11px', fontSize: '20px' }}>{name}</p>
+          <p style={{ marginLeft: '12px', fontSize: '10px' }}>{info}</p>
         </div>
-    );
+        <p
+          style={{
+            width: '80%',
+            fontFamily: 'BMJUA',
+            marginLeft: '10%',
+            margin: '0',
+            fontSize: '3vw',
+          }}
+        >
+          {src}
+        </p>
+      </div>
+    </div>
+  );
 }
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,65 +4,63 @@ import InfoLine from '../components/InfoLine';
 import test from '/logo_lion.png';
 import test2 from '/test1.jpg';
 import bg from '/bg_info.png';
-import {
-  Container,
-} from '../styles/ClubList';
+import { Container } from '../styles/ClubList';
 import center from '/logo_center.png';
 export default function About() {
   return (
-    <Container style={{backgroundImage : {bg}}}>
-    <BgAbout>
-      <Header />
-      <div style={{height : '40px'}}></div>
-      <img src='/colla.png'></img>
-      <div style={{height : '20px'}}></div>
-      <h1>총동연 회장</h1>
-      <InfoLine
-        name="송유진"
-        info="아동학과 21"
-        src="'서로' 총동아리 연합회가 응원하겠습니다"
-        img={center}
-      />
-      <h1>PM</h1>
-      <InfoLine
-        name="황현진"
-        info="컴퓨터공학과 20"
-        src="멋쟁이 사자 화이팅!"
-        img={center}
-      />
-      <h1>DESIGN</h1>
-      <InfoLine
-        name="민동명"
-        info="컴퓨터공학과 22"
-        src="멋쟁이 사자 화이팅!"
-        img={test}
-      />
-      <InfoLine
-        name="서현은"
-        info="컴퓨터공학과 21"
-        src="멋쟁이 사자 화이팅!"
-        img={test}
-      />
-      <h1>DEVELOPERS</h1>
-      <InfoLine
-        name="유창호"
-        info="컴퓨터공학과 17"
-        src="멋쟁이 사자 화이팅!"
-        img={test}
-      />
-      <InfoLine
-        name="고승범"
-        info="소프트웨어 공학과 18"
-        src="이 페이지를 시작으로 앞으로 다른사람들에게 많은 도움이 되는 사람이 되었으면 좋겠습니다 "
-        img={test}
-      />
-      <InfoLine
-        name="김효중"
-        info="컴퓨터공학과 19"
-        src="멋쟁이 사자 화이팅!"
-        img={test}
-      />
-    </BgAbout>
+    <Container style={{ backgroundImage: { bg } }}>
+      <BgAbout>
+        <Header />
+        <div style={{ height: '40px' }}></div>
+        <img src="/colla.png"></img>
+        <div style={{ height: '20px' }}></div>
+        <h1>총동연 회장</h1>
+        <InfoLine
+          name="송유진"
+          info="아동학과 21"
+          src="'서로' 총동아리 연합회가 응원하겠습니다"
+          img={center}
+        />
+        <h1>PM</h1>
+        <InfoLine
+          name="황현진"
+          info="컴퓨터공학과 20"
+          src="멋쟁이 사자 화이팅!"
+          img={center}
+        />
+        <h1>DESIGN</h1>
+        <InfoLine
+          name="민동명"
+          info="컴퓨터공학과 22"
+          src="멋쟁이 사자 화이팅!"
+          img={test}
+        />
+        <InfoLine
+          name="서현은"
+          info="컴퓨터공학과 22"
+          src="멋쟁이 사자 화이팅!"
+          img={test}
+        />
+        <h1>DEVELOPERS</h1>
+        <InfoLine
+          name="유창호"
+          info="컴퓨터공학과 20"
+          src="멋쟁이 사자 화이팅!"
+          img={test}
+        />
+        <InfoLine
+          name="고승범"
+          info="소프트웨어학과 18"
+          src="이 페이지를 시작으로 앞으로 다른사람들에게 많은 도움이 되는 사람이 되었으면 좋겠습니다 "
+          img={test}
+        />
+        <InfoLine
+          name="김효중"
+          info="컴퓨터공학과 19"
+          src="멋쟁이 사자 화이팅!"
+          img={test}
+        />
+      </BgAbout>
     </Container>
   );
 }
