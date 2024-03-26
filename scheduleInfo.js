57487b3365b64a8510d66f4b0f15ded15e98065e
changhoyu <tyu9817@naver.com>
2024-03-27 00:56:24 +0900

    last

--- scheduleInfo.js
+++ scheduleInfo.js
@@ -21,54 +21,61 @@ export const scheduleInfo = [
     time: '13:00~13:20',
     img: center,
     sns: '인스타그램 (@likelion_skuniv)',
-  },{
+  },
+  {
     id: '3',
     name: 'SDR',
     time: '13:30~14:20',
     img: sdr,
     sns: '인스타그램 (@likelion_skuniv)',
-  },{
+  },
+  {
     id: '4',
     name: '유드림',
     time: '14:20~15:00',
     img: udream,
     sns: '인스타그램 (@likelion_skuniv)',
-  },{
+  },
+  {
     id: '5',
     name: '휴식 및 이벤트',
     time: '15:00~15:20',
     img: center,
     sns: '인스타그램 (@likelion_skuniv)',
-  },{
+  },
+  {
     id: '6',
     name: '그라미',
     time: '15:20~16:10',
     img: gramy,
     sns: '인스타그램 (@likelion_skuniv)',
-  },{
+  },
+  {
     id: '7',
     name: '애니문',
     time: '16:10~16:30',
     img: aniMoon,
     sns: '인스타그램 (@likelion_skuniv)',
-  },{
+  },
+  {
     id: '8',
     name: 'WORKERS',
     time: '16:30~17:20',
     img: workers,
     sns: '인스타그램 (@likelion_skuniv)',
-  },{
+  },
+  {
     id: '9',
     name: '이벤트 뽑기',
     time: '17:20~17:50',
     img: center,
     sns: '인스타그램 (@likelion_skuniv)',
-  },{
+  },
+  {
     id: '10',
     name: '해오름제',
-    time: '17:20~17:50',
+    time: '18:00~',
     img: center,
     sns: '인스타그램 (@likelion_skuniv)',
   },
-
 ];
--- src/components/TimeLine.jsx
+++ src/components/TimeLine.jsx
@@ -1,13 +1,37 @@
-export default function TimeLine({ name, time, img }) 
-{
-    return(
-        
-        <div style={{ display: 'flex', width: '100%', height: '30px', alignItems: 'center', justifyContent: 'flex-start' ,margin:'11px 10px'}}>
-            <div style={{ display: 'flex', width: 'auto', alignItems: 'center' ,margin:'0px'}}>
-                <img src={img} style={{ width: '35px', height : '38px',marginLeft: '10px',clipPath:'circle(45%)' }} />
-                <p style={{ marginLeft: '25px' }}>{time}</p>
-            </div>
-            <p style={{ width: '50%', textAlign: 'center', marginRight : '3px' }}>{name}</p>
-        </div> 
-    );
+export default function TimeLine({ name, time, img }) {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        width: '100%',
+        height: '30px',
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+        margin: '11px 10px',
+      }}
+    >
+      <div
+        style={{
+          display: 'flex',
+          width: 'auto',
+          alignItems: 'center',
+          margin: '0px',
+        }}
+      >
+        <img
+          src={img}
+          style={{
+            width: '35px',
+            height: '38px',
+            marginLeft: '10px',
+            clipPath: 'circle(45%)',
+          }}
+        />
+        <p style={{ marginLeft: '25px', width: '90px' }}>{time}</p>
+      </div>
+      <p style={{ width: '50%', textAlign: 'center', marginRight: '3px' }}>
+        {name}
+      </p>
+    </div>
+  );
 }
