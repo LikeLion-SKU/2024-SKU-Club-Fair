715dacf417ba0f61162efd56682687e5978ee901
changhoyu <tyu9817@naver.com>
2024-03-27 15:48:45 +0900

    pc layout last

--- src/components/InfoLine.jsx
+++ src/components/InfoLine.jsx
@@ -1,52 +1,21 @@
+import {
+  InfoBlockContainer,
+  InfoContainer,
+  NameBlockContainer,
+  OneWord,
+} from '../styles/page_about';
+
 export default function InfoLine({ name, src, img, info }) {
   return (
-    <div style={{ width: '90%' }}>
-      <div
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          width: '100%',
-          height: '90px',
-          paddingTop : '5px',
-          justifyContent: 'center',
-          backgroundImage: "url('/box_about.png')",
-          backgroundSize: '100% 100%',
-        }}
-      >
-        <div
-          style={{
-            display: 'flex',
-            flexDirection: 'row',
-            width: '100%',
-            height: '30px',
-            alignItems: 'center',
-            paddingLeft: '15px',
-          }}
-        >
-          <img
-            src={img}
-            style={{
-              width: '29px',
-              height: '29px',
-              marginLeft: '15px',
-              clipPath: 'circle(50%)',
-            }}
-          />
-          <p style={{ marginLeft: '11px', fontSize: '20px', fontFamily: 'SUIT Variable', fontWeight: 'bold' }}>{name}</p>
-          <p style={{ marginLeft: '10px', fontSize: '10px' }}>{info}</p>
-        </div>
-        <p
-          style={{
-            paddingLeft: '69px',
-            paddingRight: '10px',
-            fontFamily: 'BMJUA',
-            margin: '2px',
-            fontSize: '3vw',
-          }}
-        >
-          {src}
-        </p>
-      </div>
-    </div>
+    <InfoContainer>
+      <InfoBlockContainer>
+        <NameBlockContainer>
+          <img src={img} />
+          <p>{name}</p>
+          <p>{info}</p>
+        </NameBlockContainer>
+        <OneWord>{src}</OneWord>
+      </InfoBlockContainer>
+    </InfoContainer>
   );
 }
--- src/styles/page_about.jsx
+++ src/styles/page_about.jsx
@@ -5,12 +5,12 @@ export const BgAbout = styled.div`
     flex-direction: column;
     align-items: center;
     h1{
-        font-family: "Black Han Sans",
+        font-family: Black Han Sans,
         font-weight: 400,
-        font-style: 'normal',
+        font-style: normal,
     }
     p{
-        font-family: 'SUIT Variable', sans-serif;
+        font-family: SUIT Variable, sans-serif;
         word-break: keep-all;
     }
     @media screen and (max-width: 500px) {
@@ -22,17 +22,27 @@ export const BgAbout = styled.div`
         background-size: cover;
         background-attachment: fixed;
         h1{
-            font-family: "Black Han Sans",
+            font-family: Black Han Sans,
             font-weight: 400,
-            font-style: 'normal',
+            font-style: normal,
         }
         p{
-            font-family: 'SUIT Variable', sans-serif;
+            font-family: SUIT Variable, sans-serif;
             word-break: keep-all;
         }
     }
 `;
 export const Colla = styled.div`
+  width: 228px;
+  height: 78px;
+  margin: 0px 0px 0px 0px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-image: url('/collabo.svg');
+  background-size: cover;
+  background-repeat: no-repeat;
   @media screen and (max-width: 500px) {
     width: 228px;
     height: 78px;
@@ -44,5 +54,97 @@ export const Colla = styled.div`
     background-image: url('/collabo.svg');
     background-size: cover;
     background-repeat: no-repeat;
+  }
+`;
+
+export const InfoContainer = styled.div`
+  width: 30%;
+  @media screen and (max-width: 500px) {
+    width: 90%;
+  }
+`;
+
+export const InfoBlockContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 90px;
+  padding-top: 5px;
+  justify-content: center;
+  background-image: url('/box_about.png');
+  background-size: 100% 100%;
+  @media screen and (max-width: 500px) {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    height: 90px;
+    padding-top: 5px;
+    justify-content: center;
+    background-image: url('/box_about.png');
+    background-size: 100% 100%;
+  }
+`;
+
+export const NameBlockContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  height: 30px;
+  align-items: center;
+  padding-left: 15px;
+  & img {
+    width: 29px;
+    height: 29px;
+    margin-left: 15px;
+    clip-path: circle(50%);
+  }
+  & p {
+    margin-left: 11px;
+    font-size: 20px;
+    font-family: SUIT Variable;
+    font-weight: bold;
+  }
+  & p:last-child {
+    margin-left: 10px;
+    font-size: 10px;
+  }
+  @media screen and (max-width: 500px) {
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+    height: 30px;
+    align-items: center;
+    padding-left: 15px;
+    & img {
+      width: 29px;
+      height: 29px;
+      margin-left: 15px;
+      clip-path: circle(50%);
+    }
+    & p {
+      margin-left: 11px;
+      font-size: 20px;
+      font-family: SUIT Variable;
+      font-weight: bold;
+    }
+    & p:last-child {
+      margin-left: 10px;
+      font-size: 10px;
+    }
+  }
+`;
+
+export const OneWord = styled.p`
+  padding-left: 69px;
+  padding-right: 10px;
+  font-family: BMJUA;
+  margin: 2px;
+  font-size: 5%;
+  @media screen and (max-width: 500px) {
+    padding-left: 69px;
+    padding-right: 10px;
+    font-family: BMJUA;
+    margin: 2px;
+    font-size: 3vw;
   }
 `;
